a1f629d9b8f7117a84bf77c1c2bfc7961ea9db88
LuisCoa <63749857+LuisCoa-12@users.noreply.github.com>
2023-07-08 13:34:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,10 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 # Loading dataset 
-df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
-                names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
+df = pd.read_csv('https://github.com/LuisCoa-12/apf3_itd/raw/main/Residuos_municipales_generados_anualmente.csv',
+                names=['UBIGEO','POB_TOTAL','POB_URBANA','POB_RURAL','GPC_DOM','QRESIDUOS_DOM','QRESIDUOS_NO_DOM','QRESIDUOS_MUN','PERIODO'])
 
-st.title('Exploratory Data Analysis of the Iris Dataset')
+st.title('Dataset de residuos municipales geneados anualmente')
 st.header('This app allows you to explore the Iris dataset and visualize the data using various plots.')
 
 st.subheader("DataFrame")
